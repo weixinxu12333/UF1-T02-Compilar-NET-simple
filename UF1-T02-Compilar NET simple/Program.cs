3d937409997f6b5e1192d01525334c9cee8fd72b
Weixin Xu <wexu2021daw@protomail.com>
2020-12-22 13:46:27 +0100

    Agregar archivos de proyecto.

---
+++ UF1-T02-Compilar NET simple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UF1_T02_Compilar_NET_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
